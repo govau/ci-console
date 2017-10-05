df40327cc5de837a0aad10b9cf96705a147f7f87
Adam Eijdenberg <adam.eijdenberg@digital.gov.au>
2017-10-05 15:32:19 +1100

    Install Go / Node deps, run some tests

--- ci/test.sh
+++ ci/test.sh
@@ -6,6 +6,13 @@ set -x
 # Symlink our source dir from inside of the GOPATH
 mkdir -p $GOPATH/src/github.com/18F
 ln -s $PWD/govau-cg-dashboard $GOPATH/src/github.com/18F/cg-dashboard
-
 cd $GOPATH/src/github.com/18F/cg-dashboard
+
+# Install go deps
+glide install
+
+# Install Node deps
+npm install
+
+# Run Go tests
 ./codecheck.sh
