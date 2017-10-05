60769e2d1ddf41fa116c7329f13c1530dcc3cad9
Adam Eijdenberg <adam.eijdenberg@digital.gov.au>
2017-10-05 15:15:40 +1100

    Symlink dashboard before running tests

---
+++ ci/test.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -e
+set -x
+
+# Symlink our source dir from inside of the GOPATH
+mkdir -p $GOPATH/src/github.com/18F
+ln -s $PWD/govau-cg-dashboard $GOPATH/src/github.com/18F/cg-dashboard
+
+cd $GOPATH/src/github.com/18F/cg-dashboard
+./codecheck.sh
