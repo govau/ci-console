354737b2611200489b66ab94f383fc41d1a5adb9
Adam Eijdenberg <adam.eijdenberg@digital.gov.au>
2017-10-05 15:19:14 +1100

    Fix names

--- ci/test.sh
+++ ci/test.sh
@@ -5,7 +5,7 @@ set -x
 
 # Symlink our source dir from inside of the GOPATH
 mkdir -p $GOPATH/src/github.com/18F
-ln -s $PWD/govau-cg-dashboard $GOPATH/src/github.com/18F/cg-dashboard
+ln -s $PWD $GOPATH/src/github.com/18F/cg-dashboard
 
 cd $GOPATH/src/github.com/18F/cg-dashboard
 ./codecheck.sh
