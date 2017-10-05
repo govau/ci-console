22d13c32cf406d9d57114d721a6caaf46f68c1f3
Adam Eijdenberg <adam.eijdenberg@digital.gov.au>
2017-10-05 16:09:39 +1100

    Cache all the things

--- ci/test.sh
+++ ci/test.sh
@@ -3,10 +3,24 @@
 set -e
 set -x
 
-# Symlink our source dir from inside of the GOPATH
-mkdir -p $GOPATH/src/github.com/18F
-ln -s $PWD/govau-cg-dashboard $GOPATH/src/github.com/18F/cg-dashboard
-cd $GOPATH/src/github.com/18F/cg-dashboard
+ORIG_PWD="${PWD}"
+
+# Create our own GOPATH
+export GOPATH="${ORIG_PWD}/go"
+
+# Symlink our source dir from inside of our own GOPATH
+mkdir -p "${GOPATH}/src/github.com/18F"
+ln -s "${ORIG_PWD}/govau-cg-dashboard" "${GOPATH}/src/github.com/18F/cg-dashboard"
+cd "${GOPATH}/src/github.com/18F/cg-dashboard"
+
+# Cache glide deps
+export GLIDE_HOME="${ORIG_PWD}/govau-cg-dashboard/.glide_cache"
+mkdir -p "${GLIDE_HOME}"
+
+# Cache npm deps
+NPM_CACHE="${ORIG_PWD}/govau-cg-dashboard/.npm_cache"
+mkdir -p "${NPM_CACHE}"
+npm config set cache "${NPM_CACHE}"
 
 # Install go deps
 glide install
