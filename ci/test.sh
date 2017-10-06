81c7ac4f9c20fc13ed5ffc0281810e41d869ea51
Adam Eijdenberg <adam.eijdenberg@digital.gov.au>
2017-10-06 12:00:30 +1100

    Build NPM app

--- ci/test.sh
+++ ci/test.sh
@@ -17,16 +17,32 @@ cd "${GOPATH}/src/github.com/18F/cg-dashboard"
 export GLIDE_HOME="${ORIG_PWD}/govau-cg-dashboard/.glide_cache"
 mkdir -p "${GLIDE_HOME}"
 
+# Install go deps
+glide install
+
+# Run Go tests - skip Docker tests as we are not in privileged mode
+SKIP_DOCKER=1 ./codecheck.sh
+
+# Install skin requirements
+export YARN_CACHE_FOLDER=${ORIG_PWD}/govau-ci-console/skin/.yarn_cache
+mkdir -p "${YARN_CACHE_FOLDER}"
+cd "${ORIG_PWD}/govau-ci-console/skin"
+yarn install
+
+# Symlink it into the dashboard dir
+ln -s ${ORIG_PWD}/govau-ci-console/skin ${ORIG_PWD}/govau-cg-dashboard/static_src/skins/govau
+ln -s ${ORIG_PWD}/govau-ci-console/skin ${ORIG_PWD}/govau-cg-dashboard/static/skins/govau
+
+# Build and deploy the frontend
+cd ${ORIG_PWD}/govau-cg-dashboard
+
 # Cache npm deps
 NPM_CACHE="${ORIG_PWD}/govau-cg-dashboard/.npm_cache"
 mkdir -p "${NPM_CACHE}"
 npm config set cache "${NPM_CACHE}"
 
-# Install go deps
-glide install
-
 # Install Node deps
 npm install
 
-# Run Go tests - skip Docker tests as we are not in privileged mode
-SKIP_DOCKER=1 ./codecheck.sh
+# Build it
+NODE_ENV="prod" SKIN_NAME="govau" SKIN_PROVIDES_TRANSLATIONS="true" npm run build-prod
