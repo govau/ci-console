1e9bd949895bae56a752749baeeba69ac5cc2ac1
Jonathan Ingram <jonathan.ingram@digital.gov.au>
2017-11-03 10:28:30 +1100

    skin: remove TODO now babel is upgraded

--- skin/index.js
+++ skin/index.js
@@ -1,5 +1,3 @@
 import "./img/favicon.png";
 
-// TODO(jonathaningram): use `export * from "./src";` when babel is upgraded.
-export { default } from "./src";
-export { lang, config, homePage, header } from "./src";
+export * from "./src";
